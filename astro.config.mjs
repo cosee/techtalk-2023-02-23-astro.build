9b2c856f5676a9867d6894f941935b93cc5443a1
Nils Knappmeier <nils.knappmier@cosee.biz>
2023-02-19 15:21:52 +0100

    static site generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
 
 // https://astro.build/config
 import react from "@astrojs/react";
@@ -10,11 +9,5 @@ import solidJs from "@astrojs/solid-js";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), solidJs()],
-  // Run in a server
-  output: "server",
-  adapter: node({
-    mode: 'standalone'
-  }),
-  // Generate static pages
-  // output: "static"
+  output: "static"
 });
